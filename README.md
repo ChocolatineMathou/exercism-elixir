88a1a7a2b9ce330e2f9a0061ec66e1e2ab395f40
Mathilde Ferrand Coeurderoy <mathilde.ferrand.coeurderoy@gmail.com>
2018-11-25 14:56:21 +0000

    add two first exercises

---
+++ README.md
@@ -0,0 +1,7 @@
+# Learning Elixir
+
+I recently decided to learn functional programming and picked Elixir.   
+These are the exercises I solved on [Exercism](https://exercism.io).
+
+* Hello world
+* Bob
---
+++ bob/bob.exs
@@ -0,0 +1,27 @@
+defmodule Bob do
+  def hey(input) do
+    cond do
+      capitalquestion?(input) -> "Calm down, I know what I'm doing!"
+      question?(input) -> "Sure."
+      silence?(input) -> "Fine. Be that way!"
+      shouting?(input) -> "Whoa, chill out!"
+      true -> "Whatever."
+    end
+  end
+
+  defp shouting?(input) do
+    String.upcase(input) == input && String.downcase(input) != input
+  end
+
+  defp silence?(input) do
+    String.trim(input) == ""
+  end
+
+  defp question?(input) do
+    String.ends_with?(input, "?")
+  end
+
+  defp capitalquestion?(input) do
+    shouting?(input) && question?(input)
+  end
+end
